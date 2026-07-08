d111e59f90b6cb30bf87701ac925d92203f46594
thelastmedici <christianopeyemijoshua@gmail.com>
2026-07-08 01:45:50 +0100

    add(sql): added query to get net revenue and sorted by month

--- motorcycle_spare_part_sale/sql/query.sql
+++ motorcycle_spare_part_sale/sql/query.sql
@@ -1,3 +1,7 @@
-SELECT *
+SELECT product_line, warehouse, to_char(date, 'FMMonth') AS month,
+       SUM(total) - SUM(payment_fee) AS net_revenue
 FROM sales
-LIMIT 10;
+-- GROUP BY month, warehouse
+WHERE client_type = 'Wholesale'
+    GROUP BY product_line, warehouse, to_char(date, 'FMMonth'), extract(month FROM date)
+ORDER BY   product_line, extract(month FROM  date), net_revenue desc;
